311ed5f84d32676e814d3658d9f808e5f13510ef
𝔸𝕝𝕚𝕟 <popovicialincatalin@gmail.com>
2026-02-26 17:04:03 +0200

    123

--- !assets/GAMA for Windows/GAMA-Core/GAMA-OpenGL/gama-opengl.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"%~dp0\adb" shell setprop debug.hwui.renderer opengl
-"%~dp0\adb" shell am crash com.android.systemui
-"%~dp0\adb" shell am force-stop com.android.settings
-"%~dp0\adb" shell am force-stop com.sec.android.app.launcher
-"%~dp0\adb" shell am force-stop com.samsung.android.app.aodservice
-"%~dp0\adb" shell am crash com.google.android.inputmethod.latin b
